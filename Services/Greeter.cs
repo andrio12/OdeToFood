a86c99b5ee957b66e3c01f655f4fe3d6c71a38b4
Andri <andriolafsson@gmail.com>
2017-03-03 12:35:30 +0000

    initial commit

---
+++ Services/Greeter.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood.Services
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["greeting"];
+        }
+        string IGreeter.GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
